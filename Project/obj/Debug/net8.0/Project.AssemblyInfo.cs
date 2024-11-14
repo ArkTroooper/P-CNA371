2db9d2da39ff78e58dcaac18497b2b1513256f2b
Aaron Krishna <aaron.krishna@laysholdings.co.za>
2024-11-14 09:53:33 +0200

    Deleted the bash-log file, couldn't use it to show a commit tree for docker images

--- Project/obj/Debug/net8.0/Project.AssemblyInfo.cs
+++ Project/obj/Debug/net8.0/Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10fbc89461fb102cd6640061286ca8c90f590952")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+541efd588c0341f778880bbe48e42e977b5910ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
